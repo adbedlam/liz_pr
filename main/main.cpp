f8172eeb5602c658b745b49c102a4d90691ee6d9
Nikita Yaneev <nikita.yaneev@yandex.ru>
2025-03-11 20:14:09 +0300

    add CMake

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Инициализация GLFW
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    // Настройка окна
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Создание окна
+    GLFWwindow* window = glfwCreateWindow(800, 600, "dasljk", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Инициализация GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+
+    // Главный цикл
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.9f, 0.0f, 0.9f, 0.5f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
